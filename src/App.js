c70eb59a45f1e602fbdee0f907b78cdcbd0a6fb3
Akolyte <hryoo2004@gmail.com>
2023-10-24 18:01:34 -0400

    feat: moved form to its own component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 let response;
+
+function ChatGptFormComponent() {
+  return (
+    <form>
+      <div class="form-wrapper">
+        <textarea id="textInput" placeholder="Ask ChatGPT a question!" rows="5" cols="50"></textarea>
+        <button type="submit" id="submitButton">Submit</button>
+      </div>
+    </form>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <form>
-          <div class="form-wrapper">
-            <textarea id="textInput" placeholder="Ask ChatGPT a question!" rows="5" cols="50"></textarea>
-            <button type="submit" id="submitButton">Submit</button>
-          </div>
-        </form>
+        <ChatGptFormComponent />
         <p>{response}</p>
       </header>
     </div>
